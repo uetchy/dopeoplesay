95a8764a23beb4ccceeb061926ec14988226394d
Yasuaki Uechi <y@uechi.io>
2019-01-24 17:02:36 +0900

    use src

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,5 +1,5 @@
 import { stdout } from 'test-console'
-import * as lib from '../lib/lib'
+import * as lib from '../src/lib'
 
 test('makeURL', () => {
   expect(lib.makeURL('a b')).toBe('https://dopeoplesay.com/q/a%20b')
