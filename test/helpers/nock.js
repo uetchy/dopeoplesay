e450aad7ef222d2f9fbe5d701995e8ff3ad3d07a
Yasuaki Uechi <y@uechi.io>
2019-02-12 15:16:36 +0900

    add mock test

---
+++ test/helpers/nock.js
@@ -0,0 +1,15 @@
+import nock from 'nock'
+import { resolve } from 'path'
+
+export default function declareMockScope() {
+  nock.disableNetConnect()
+
+  const scope = nock('https://dopeoplesay.com')
+    .get(/\/q\/+/)
+    .replyWithFile(200, resolve(__dirname, '../fixtures/search-test.txt'))
+
+  return function cleanAll() {
+    nock.cleanAll()
+    nock.enableNetConnect()
+  }
+}
--- test/lib.test.js
+++ test/lib.test.js
@@ -1,6 +1,29 @@
-import { stdout } from 'test-console'
+// import { stdout } from 'test-console'
+import nockHelper from './helpers/nock'
 import * as lib from '../src/lib'
 
+let cleaner
+
+beforeEach(() => {
+  cleaner = nockHelper()
+})
+
+afterEach(() => {
+  cleaner()
+})
+
 test('makeURL', () => {
-  expect(lib.makeURL('a b')).toBe('https://dopeoplesay.com/q/a%20b')
+  const url = lib.makeURL('a b')
+  expect(url).toBe('https://dopeoplesay.com/q/a%20b')
+})
+
+test('fetchDOM', async () => {
+  const dom = await lib.fetchDOM('test')
+  expect(dom).toHaveProperty('window')
+})
+
+test('parse', async () => {
+  const dom = await lib.fetchDOM('test')
+  const result = await lib.parse(dom)
+  expect(Object.keys(result)).toEqual(['definitions', 'collocations'])
 })
